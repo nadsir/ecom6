9419106ca8a20122834c3c71c67b5010d65fbc0f
nadsir <nader.davarmanesh62@gmail.com>
2021-01-29 22:30:05 -0800

    lesson 53 make brand migration and model and seeder for brand table in database

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -8,6 +8,8 @@ use App\Category;
 use App\Product;
 use App\ProductsAttribute;
 use App\ProductsImage;
+use App\Brand;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -18,49 +20,58 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
-/*        Admin::truncate();
-        Section::truncate();
-        User::truncate();
-        factory(App\Admin::class,10)->create();*/
+        /*        Admin::truncate();
+                Section::truncate();
+                User::truncate();
+                factory(App\Admin::class,10)->create();*/
         /*factory(App\Section::class,10)->create();*/
-/*        factory(App\User::class,10)->create();*/
+        /*        factory(App\User::class,10)->create();*/
 
-/*        $sectionRecord=[
-            ['id'=>1,'name'=>'Men','status'=>1],
-            ['id'=>2,'name'=>'Women','status'=>1],
-            ['id'=>3,'name'=>'Kids','status'=>1],
-        ];
-        Section::insert($sectionRecord);*/
-/*        $categoryRecords=[
-            ['id'=>1,'parent_id'=>0,'section_id'=>1,'category_name'=>'T-Shirts','category_image'=>'','category_discount'=>0,'description'=>'','url'=>'t-shirt',
-                'meta_title'=>'','meta_description'=>'','meta_keywords'=>'','status'=>1],
-            ['id'=>2,'parent_id'=>1,'section_id'=>1,'category_name'=>'Casual T-Shirts','category_image'=>'','category_discount'=>0,'description'=>'','url'=>'Casual t-shirt',
-                'meta_title'=>'','meta_description'=>'','meta_keywords'=>'','status'=>1],
-        ];
-        Category::insert($categoryRecords);*/
-/*        $productRecords=[
-            ['id'=>1,'category_id'=>4,'section_id'=>1,'product_name'=>'Blue Casual T-Shirt','product_code'=>'BT001','product_color'=>'Blue','product_price'=>'1500',
-                'product_discount'=>10,'product_weight'=>200,'product_video'=>'','main_image'=>'','description'=>'Test Product','wash_care'=>'','fabric'=>'',
-                'pattern'=>'','sleeve'=>'','fit'=>'','occassion'=>'','meta_title'=>'','meta_description'=>'','meta_keywords'=>'','is_featured'=>'No','status'=>1,
+        /*        $sectionRecord=[
+                    ['id'=>1,'name'=>'Men','status'=>1],
+                    ['id'=>2,'name'=>'Women','status'=>1],
+                    ['id'=>3,'name'=>'Kids','status'=>1],
+                ];
+                Section::insert($sectionRecord);*/
+        /*        $categoryRecords=[
+                    ['id'=>1,'parent_id'=>0,'section_id'=>1,'category_name'=>'T-Shirts','category_image'=>'','category_discount'=>0,'description'=>'','url'=>'t-shirt',
+                        'meta_title'=>'','meta_description'=>'','meta_keywords'=>'','status'=>1],
+                    ['id'=>2,'parent_id'=>1,'section_id'=>1,'category_name'=>'Casual T-Shirts','category_image'=>'','category_discount'=>0,'description'=>'','url'=>'Casual t-shirt',
+                        'meta_title'=>'','meta_description'=>'','meta_keywords'=>'','status'=>1],
+                ];
+                Category::insert($categoryRecords);*/
+        /*        $productRecords=[
+                    ['id'=>1,'category_id'=>4,'section_id'=>1,'product_name'=>'Blue Casual T-Shirt','product_code'=>'BT001','product_color'=>'Blue','product_price'=>'1500',
+                        'product_discount'=>10,'product_weight'=>200,'product_video'=>'','main_image'=>'','description'=>'Test Product','wash_care'=>'','fabric'=>'',
+                        'pattern'=>'','sleeve'=>'','fit'=>'','occassion'=>'','meta_title'=>'','meta_description'=>'','meta_keywords'=>'','is_featured'=>'No','status'=>1,
 
-                ],
-            ['id'=>2,'category_id'=>4,'section_id'=>1,'product_name'=>'Red Casual T-Shirt','product_code'=>'RT001','product_color'=>'Red','product_price'=>'2000',
-                'product_discount'=>10,'product_weight'=>200,'product_video'=>'','main_image'=>'','description'=>'Test Product','wash_care'=>'','fabric'=>'',
-                'pattern'=>'','sleeve'=>'','fit'=>'','occassion'=>'','meta_title'=>'','meta_description'=>'','meta_keywords'=>'','is_featured'=>'Yes','status'=>1,
+                        ],
+                    ['id'=>2,'category_id'=>4,'section_id'=>1,'product_name'=>'Red Casual T-Shirt','product_code'=>'RT001','product_color'=>'Red','product_price'=>'2000',
+                        'product_discount'=>10,'product_weight'=>200,'product_video'=>'','main_image'=>'','description'=>'Test Product','wash_care'=>'','fabric'=>'',
+                        'pattern'=>'','sleeve'=>'','fit'=>'','occassion'=>'','meta_title'=>'','meta_description'=>'','meta_keywords'=>'','is_featured'=>'Yes','status'=>1,
 
-            ],
-        ];
-        Product::insert($productRecords);*/
-/*        $productAttributeRecords=[
-            ['id'=>1,'product_id'=>10,'size'=>'small','price'=>1200,'stock'=>10,'sku'=>'GCT001-S','status'=>1],
-            ['id'=>2,'product_id'=>10,'size'=>'small','price'=>1300,'stock'=>20,'sku'=>'GCT001-M','status'=>1],
-            ['id'=>3,'product_id'=>10,'size'=>'small','price'=>1400,'stock'=>10,'sku'=>'GCT001-L','status'=>1],
-        ];
-        ProductsAttribute::insert($productAttributeRecords);*/
+                    ],
+                ];
+                Product::insert($productRecords);*/
+        /*        $productAttributeRecords=[
+                    ['id'=>1,'product_id'=>10,'size'=>'small','price'=>1200,'stock'=>10,'sku'=>'GCT001-S','status'=>1],
+                    ['id'=>2,'product_id'=>10,'size'=>'small','price'=>1300,'stock'=>20,'sku'=>'GCT001-M','status'=>1],
+                    ['id'=>3,'product_id'=>10,'size'=>'small','price'=>1400,'stock'=>10,'sku'=>'GCT001-L','status'=>1],
+                ];
+                ProductsAttribute::insert($productAttributeRecords);*/
 
-        $productImageRecords=[
-            ['id'=>1,'product_id'=>1,'image'=>'Noizy-Wood-black-tshirt-white-X-48332.jpg','status'=>1]
+/*        $productImageRecords = [
+            ['id' => 1, 'product_id' => 1, 'image' => 'Noizy-Wood-black-tshirt-white-X-48332.jpg', 'status' => 1]
         ];
-        ProductsImage::insert($productImageRecords);
+        ProductsImage::insert($productImageRecords);*/
+
+        $brandRecords = [
+            ['id' => 1, 'name' => 'Arrow', 'status' =>1],
+            ['id' => 2, 'name' => 'Gap', 'status' =>1],
+            ['id' => 3, 'name' => 'Lee', 'status' =>1],
+            ['id' => 4, 'name' => 'Monte Carlo', 'status' =>1],
+            ['id' => 5, 'name' => 'Peter England', 'status' =>1],
+        ];
+        Brand::insert($brandRecords);
     }
 }
